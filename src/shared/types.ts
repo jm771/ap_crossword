e2396cd744f70e3e306a5fb742e0ba9cac107a71
JackMills <JackMills@NotRealEmail.com>
2026-05-25 10:34:58 -0400

    more types

--- src/shared/types.ts
+++ src/shared/types.ts
@@ -27,3 +27,32 @@ export type RandomizerStateJson = {
 export interface GameJson {
   randomizer?: RandomizerStateJson;
 }
+
+export type Direction = "Across" | "Down"
+
+export interface ClueId {
+  direction: Direction
+  number: number
+}
+
+export interface Clue {
+  direction: Direction
+  number: number
+  clue: string
+  answer: string
+}
+
+export interface CrossLetter {
+  clue_id: ClueId
+  index: number
+  value: string
+
+}
+
+export interface SlotData {
+  n_starting_clues: number
+  clues_per_reward: number
+  cross_letters_per_reward: number
+  clues: Clue[]
+  cross_letters: CrossLetter[]
+}
