4db14c216795c6ecb0dc2f3d49c7c4152405e493
JackMills <JackMills@NotRealEmail.com>
2026-05-27 20:26:35 -0400

    correct bast

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/randomizer-game/',
+  base: '/ap_crossword/',
   server: {
     port: 3020,
   },
