d0c1bc6a52506dc5354e223d79fdb36cee858272
JackMills <JackMills@NotRealEmail.com>
2026-06-12 18:56:51 +0100

    Revert "Merge branch 'ClueBoxes'"
    This reverts commit 0438944bf27868e79ef5b2e8cedb2f2593747be2, reversing
    changes made to a7e2093db063c979be0a58f46d5984dfa7a8b9ee.

--- src/components/RandomizerGame.tsx
+++ src/components/RandomizerGame.tsx
@@ -1,17 +1,13 @@
-import {
-  RewardsState,
-  clue_id_to_string,
-  clue_id_to_loc_id,
-} from "../shared/types";
-import { Box } from "@mui/material";
-import { ClientHandler } from "../archipelago_client_handler";
-import { ClueCard } from "./ClueCard";
+import {RewardsState, clue_id_to_string, clue_id_to_loc_id} from '../shared/types';
+import {Box} from '@mui/material';
+import { ClientHandler } from '../archipelago_client_handler';
+import { ClueCard } from './ClueCard';
 
 // Remove?
 interface RandomizerGameProps {
-  client: ClientHandler;
-  rewards: RewardsState;
-  visitedLocations: Set<number>;
+  client: ClientHandler,
+  rewards: RewardsState,
+  visitedLocations: Set<number>,
 }
 
 class DefaultMap<K, V> extends Map<K, V> {
@@ -28,44 +24,30 @@ class DefaultMap<K, V> extends Map<K, V> {
   }
 }
 
-export function RandomizerGame({
-  client,
-  rewards,
-  visitedLocations,
-}: RandomizerGameProps) {
-  const slotdata = client.getSlotData();
-  const nCrossLetters = Math.floor(
-    slotdata.cross_letters_per_reward * rewards.nCrossLetterRewards,
-  );
-  const nRevealedClues =
-    slotdata.n_starting_clues +
-    Math.floor(slotdata.clues_per_reward * rewards.nClueRewards);
-  const revealedLetterIndicies = new DefaultMap<string, number[]>(() => []);
-  slotdata.cross_letters.slice(0, nCrossLetters).forEach((cl) => {
-    revealedLetterIndicies
-      .getOrCreate(clue_id_to_string(cl.clue_id))
-      .push(cl.index);
-  });
 
-  return (
-    <Box className="clues-container" p={2}>
-      {slotdata.clues.map((clue, index) => {
-        const isSolved = visitedLocations.has(clue_id_to_loc_id(clue));
-        const isUncensored = isSolved || index < nRevealedClues;
-        const clueIdStr = clue_id_to_string(clue);
-        const lettersForClue = revealedLetterIndicies.getOrCreate(clueIdStr);
 
-        return (
-          <ClueCard
-            key={clueIdStr}
-            client={client}
-            clue={clue}
-            isSolved={isSolved}
-            isUncensored={isUncensored}
-            revealedLetters={lettersForClue}
-          />
-        );
-      })}
-    </Box>
-  );
+
+
+export function RandomizerGame({client, rewards, visitedLocations}: RandomizerGameProps)
+{
+    const slotdata = client.getSlotData();
+    const nCrossLetters = Math.floor(slotdata.cross_letters_per_reward * rewards.nCrossLetterRewards);
+    const nRevealedClues = slotdata.n_starting_clues + Math.floor(slotdata.clues_per_reward * rewards.nClueRewards);
+    const revealedLetterIndicies = new DefaultMap<string, number[]>(() => []);
+    slotdata.cross_letters.slice(0, nCrossLetters).forEach(cl => {
+      revealedLetterIndicies.getOrCreate(clue_id_to_string(cl.clue_id)).push(cl.index);
+    });
+
+    return (
+        <Box className="clues-container" p={2}>
+          {slotdata.clues.map((clue, index) => {
+            const isSolved = visitedLocations.has(clue_id_to_loc_id(clue));
+            const isUncensored = index < nRevealedClues;
+            const clueIdStr = clue_id_to_string(clue);
+            const lettersForClue = revealedLetterIndicies.getOrCreate(clueIdStr);
+
+              return (<ClueCard key={clueIdStr} client={client} clue={clue} isSolved={isSolved} isUncensored={isUncensored} revealedLetters={lettersForClue}/>)
+          })}
+        </Box>
+    );
 }
