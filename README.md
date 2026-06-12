e3cd303fe4d698b8b66fbf44beda40e8502fa3c2
JackMills <JackMills@NotRealEmail.com>
2026-06-12 18:57:56 +0100

    Revert "Merge pull request #1 from jm771/ClueBoxes"
    This reverts commit e0fd6929ba9deae6b794a3f9b2d28d18d391424e, reversing
    changes made to 50a4cf46ca954864a1fe73bbf009e525f7713a3f.

--- README.md
+++ README.md
@@ -9,47 +9,41 @@ For that reason, the crossword grid is not visibile - the clues are presented in
 Archipelago is more fun when you can't beat the game without rewards from the multiworld, so for that reason you don't start with all the clues available. Clues are also a randomizer item. However to allow for some "out of logic fun" - you can still see the first half of clues that not unlocked.
 
 ### Your own puzzle
-
 Part of the fun of crosswords is having a high quality crossword with great clues of a style you enjoy (American style/Cryptic/Easy/ Hard) so the randomizer outsources the job of finding a good crossword to you.
 As part of world generation you need to provide a `.puz` file containing the crossword you wish to randomize. Several crossword outlets provide `.puz` files as part of a subscription. Or you can use this excellent tool to download `.puz` files from many popular outlets: https://github.com/thisisparker/xword-dl
 Archipelago community member Buck has made some free puzzles you can download here: https://buckscrosswords.xyz/archive.html
 
 ## Setup
-
 Download the latest apworld file from: https://github.com/jm771/apworld_crossword/releases/ and install it into archipelago.
 Update the sample yaml file with a pointer to your `.puz` file and generate and host a world.
 Use the website here: https://jm771.github.io/ap_crossword/ and connect to your archipelago server.
 
 ## FAQ
-
 #### Having to upload multiple files makes things harder for the person running the multiworld
-
 You can now bundle your puz file into your yaml. Upload your baseline yaml and your puz file and download the result:
 https://jm771.github.io/ap_crossword_yaml_puz/
 
 #### "while parsing a quoted scalar did not find expected hexdecimal number"
-
 yaml treats `\` as escape sequences in strings that use double quotes `"`. Unfortunately windows "copy as path" triggers this. Change your double quotes to single quotes `'`
 
 ## Feedback
-
 The game is very new. I currently have some time to put into this - and am very open to any feedback that'd help make this easier to use or more fun for people to play. I'd prioritise that over my own ideas. Feel free to contact me through github issues or on the archipelago discord under name rabidherring.
 
 ### Work in progress
-
+- Fix display of non-ascii characters
 - Text client on the web interface
 - Display puzzle info (setter, copyright, any gimick info)
 - Better visibility of what you're unlocking for people (in chat or banner)
 - Track number of wrong guesses
-- Puzzle info included in visuals
-- Generation options to skew cross letters earlier
-- Configurable level of censorship
+- Rerender less of the UI on updates
+- Better clue entry
+  - Currntly can have a typo overlap with an unlocked cross letter and it's hard to see
+  - Editing part way through clue causes cursor to jump
+  - Would be nice to enter in box directly
 
 ### Maybe in the future?
-
 - Show grid in crossword form on game completion
 - Deathlink
 
 ## Alternatives
-
-densebamboo has also made an archipelago crossword randomizer with very different design choices (the words are actually crossed, the website provides the clues rather than the user providing a `.puz` file). It doesn't appear to be under active development at the moment, but was more mature than this is when it was stopped. You might also enjoy checking it out: https://densebamboo.github.io/CrosswordAP/
+densebamboo has also made an archipelago crossword randomizer with very different design choices (the words are actually crossed, the website provides the clues rather than the user providing a `.puz` file). It doesn't appear to be under active development at the moment, but was more mature than this is when it was stopped. You might also enjoy checking it out: https://densebamboo.github.io/CrosswordAP/ 
--- src/components/RandomizerPage.tsx
+++ src/components/RandomizerPage.tsx
@@ -1,85 +1,61 @@
+
 import { useEffect, useState } from "react";
 import { ClientHandler } from "../archipelago_client_handler";
-import RandomizerConfigDialog, {
-  getInitialConfigState,
-  RandomizerConfig,
-} from "./RandomizerConfig";
+import RandomizerConfigDialog, { getInitialConfigState, RandomizerConfig } from "./RandomizerConfig";
 import { RewardsState } from "../shared/types";
 import React from "react";
 import { RandomizerGame } from "./RandomizerGame";
 import { GameHeader } from "./GameHeader";
 
 export function RandomizerPage() {
-  const [config, setConfig] = useState<RandomizerConfig>(
-    getInitialConfigState(),
-  );
-  const [configOpen, setConfigOpen] = useState<boolean>(!config.configSet);
-  const [client, setClient] = useState<ClientHandler | null>(null);
-  const [visitedLocations, setVisitedLocations] = useState<Set<number>>(
-    new Set(),
-  );
-  const [rewardState, setRewardState] = useState<RewardsState>({
-    sequenceNo: 0,
-    nClueRewards: 0,
-    nCrossLetterRewards: 0,
-  });
-  const [connectionMessage, setConnectionMessage] = useState<string>("");
-
-  useEffect(() => {
-    if (config === null) return;
-    setRewardState({
-      sequenceNo: 0,
-      nClueRewards: 0,
-      nCrossLetterRewards: 0,
+    const [config, setConfig] = useState<RandomizerConfig>(getInitialConfigState());
+    const [configOpen, setConfigOpen] = useState<boolean>(!config.configSet);
+    const [client, setClient] = useState<ClientHandler | null>(null);
+    const [visitedLocations, setVisitedLocations] = useState<Set<number>>(new Set());
+    const [rewardState, setRewardState] = useState<RewardsState>({
+        sequenceNo: 0,
+        nClueRewards: 0,
+        nCrossLetterRewards: 0
     });
-    setVisitedLocations(new Set());
+    const [connectionMessage, setConnectionMessage] = useState<string>("");
 
-    const newClient = new ClientHandler(
-      setRewardState,
-      setVisitedLocations,
-      (message) => {
-        setConnectionMessage(message);
-        setConfigOpen(true);
-      },
-    );
+    useEffect(() => {
+        if (config === null) return;
+        const newClient = new ClientHandler(
+            setRewardState,
+            setVisitedLocations,
+            (message) => {
+                setConnectionMessage(message);
+                setConfigOpen(true);
+            }
+        );
 
-    newClient.login(
-      config.archipelagoUrl,
-      config.slotName,
-      config.password,
-      () => {
-        setClient(newClient);
-        setConnectionMessage("Connected!");
-      },
-    );
+        newClient.login(config.archipelagoUrl,
+            config.slotName,
+            config.password,
+            () => {
+                setClient(newClient);
+                setConnectionMessage("Connected!");
+            });
 
-    return () => newClient.disconnect();
-  }, [config]);
+        return () => newClient.disconnect()
+    },
+    [config])
 
-  const totalClues = client?.getSlotData().clues.length ?? 0;
+    const totalClues = client?.getSlotData().clues.length ?? 0;
 
-  return (
+    return (
     <React.StrictMode>
-      <div className="randomizer-game">
+        <div className="randomizer-game">
         <RandomizerConfigDialog
-          open={configOpen}
-          onClose={() => setConfigOpen(false)}
-          onSave={setConfig}
-          connectionMessage={connectionMessage}
+            open={configOpen}
+            onClose={() => setConfigOpen(false)}
+            onSave={setConfig}
+            connectionMessage={connectionMessage}
         />
-        <GameHeader
-          totalClues={totalClues}
-          solvedCount={visitedLocations.size}
-          handleOpenConfig={() => setConfigOpen(true)}
-        />
-        {client && (
-          <RandomizerGame
-            client={client}
-            rewards={rewardState}
-            visitedLocations={visitedLocations}
-          />
-        )}
-      </div>
-    </React.StrictMode>
-  );
+            <GameHeader totalClues={totalClues} solvedCount={visitedLocations.size} handleOpenConfig={() => setConfigOpen(true) }/>
+            {client && ( <RandomizerGame client={client} rewards={rewardState} visitedLocations={visitedLocations}/>)}
+            </div>
+        </React.StrictMode>
+        );
 }
